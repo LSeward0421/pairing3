55bb25a346a9655c0dd40b7d3e620e3015dab787
Lauren Seward <lauren.unal@gmail.com>
2022-10-22 14:47:47 -0500

    Add second for statement

--- friends.js
+++ friends.js
@@ -3,3 +3,7 @@ for ( var i= 0; i< numbers.length; i++) {
     if (numbers[i] > 30) 
     console.log(numbers[i])
 }
+for (var i = 0; i<numbers.length; i++) {
+    if(numbers[i] < 60)
+    console.log(numbers[i])
+}
